96209c139cb3f61e88e37ed66d887dc26b601187
melek <khemakhem.melek123@gmail.com>
2024-09-05 22:26:50 +0100

    ajout environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA2c_f9Fb-embCHZ1CD0W_0FubRNzzxr5c",
+    authDomain: "amenyprojet.firebaseapp.com",
+    databaseURL: "https://amenyprojet-default-rtdb.firebaseio.com",
+    projectId: "amenyprojet",
+    storageBucket: "amenyprojet.appspot.com",
+    messagingSenderId: "567529180140",
+    appId: "1:567529180140:web:4f6a19df5c54c5225b8ec9",
+  }
 };
